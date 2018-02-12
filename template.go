8616e25120729e00b2deff0901de0d6c5797e1ff
Igor Rybak <irybak@travix.com>
2018-02-12 12:43:02 +0100

    add decoding template from base64

--- template.go
+++ template.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"regexp"
 	"text/template"
+
+	"github.com/rs/zerolog/log"
 )
 
 func (req *ExpectationRequest) regexLocator(filter string) interface{} {
@@ -24,14 +27,31 @@ func (req *ExpectationRequest) regexLocator(filter string) interface{} {
 
 // TemplateCreateResponseBody creates response body as string based on template and incoming request
 func TemplateCreateResponseBody(tmpl string, req *ExpectationRequest) string {
+	fLog := log.With().Str("function", "TemplateCreateResponseBody").Logger()
+
+	decodedTmpl, err := TemplateDecodeFromBase64(tmpl)
+	if err != nil {
+		fLog.Warn().Err(err)
+		decodedTmpl = tmpl
+	}
+
 	buf := new(bytes.Buffer)
 	fmap := template.FuncMap{
 		"regexLocator": req.regexLocator}
-	t := template.Must(template.New("main").Funcs(fmap).Parse(tmpl))
-	err := t.Execute(buf, req.Body)
+	t := template.Must(template.New("main").Funcs(fmap).Parse(decodedTmpl))
+	err = t.Execute(buf, req.Body)
 	if err != nil {
-		panic(err)
+		fLog.Error().Err(err)
 	}
 	res := buf.String()
 	return res
+}
+
+// TemplateDecodeFromBase64 decodes string from base64
+func TemplateDecodeFromBase64(encoded string) (string, error) {
+	decoded, err := base64.StdEncoding.DecodeString(encoded)
+	if err == nil {
+		return string(decoded), nil
+	}
+	return "", err
 }
